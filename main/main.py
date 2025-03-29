62a37df8d27d90722a19365d0899885d3cf9454a
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-03-28 21:54:27 -0500

    Clase algoritmos

--- main/main.py
+++ main/main.py
@@ -1,27 +1,7 @@
-import sys
-from pathlib import Path
-
-# Añade la ruta del proyecto al PATH de Python
-sys.path.append(str(Path(__file__).parent.parent))  # ← Sube dos niveles (a "taller_ciencias_1/")
-
-from models.Politicos import Politico
-import random
+import algoritmos
 
 n = int(input("ingrese una cantidad de politicos: "))
 
-list = []
+list = algoritmos.CreateList(n)
 
-for i in range(n):
-    valor_robo = random.randint(100, 1000000)
-    edad = random.randint(20, 60)
-    politico = Politico(i, valor_robo, edad)
-    
-    politico_dict = {
-        "id": politico.id,
-        "valor_robo": politico.valor_robo,
-        "edad": politico.edad
-    }
-    
-    list.append(politico_dict)
-
-print(list)
+algoritmos.PrintList(list)
